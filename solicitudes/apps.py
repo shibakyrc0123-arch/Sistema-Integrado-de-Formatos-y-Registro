79f2d4196bf64ab867d6db49b2e73522e5a988c8
Maria Fernanda Gonzalez Coca <Seleccion@COVICOLSAS.LOCAL>
2026-03-12 14:24:49 -0500

    Arreglo final apps y subida forzada

--- solicitudes/apps.py
+++ solicitudes/apps.py
@@ -1,8 +1,4 @@
 from django.apps import AppConfig
-from django.apps import AppConfig
-
-class SolicitudesConfig(AppConfig):
-    name = 'solicitudes'
 
 class SolicitudesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
